709b353dcf24f5fa525ce60570aa858733e9e0c1
Ayalamri <ayalamrioui19@gmail.com>
2023-12-22 10:47:18 -0800

    rtrt

---
+++ README.md
@@ -0,0 +1,1 @@
+Hi RSA-Factoring-Challenge
---
+++ factors.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * factorize - Finds and prints the factorization
+ * @num: The number to factorize
+ */
+void factorize(int num)
+{
+	int i;
+
+	for (i = 2; i <= num; ++i)
+	{
+		while (num % i == 0)
+		{
+			printf("%d=%d*", num, i);
+			num /= i;
+		}
+	}
+}
+
+/**
+ * main - Entry point of the program
+ * @argc: Number of command-line arguments
+ * @argv: Array of command-line arguments
+ * Return: 0 on success, 1 on failure
+ */
+int main(int argc, char *argv[])
+{
+	/* Check if the correct number of arguments is provided */
+	if (argc != 2)
+	{
+		fprintf(stderr, "Usage: %s <file>\n", argv[0]);
+		return (1);
+	}
+
+	FILE *file = fopen(argv[1], "r");
+
+	if (file == NULL)
+	{
+		perror("Error opening file");
+		return (1);
+	}
+
+	int num;
+
+	while (fscanf(file, "%d", &num) == 1)
+	{
+		factorize(num);
+		printf("\n");
+	}
+
+	/* Close the file */
+	fclose(file);
+
+	return (0);
+}
